7d93762886934a4789d4d84f6d797b21d3733913
devincloudkelly <devin.cloud.kelly@gmail.com>
2019-12-09 14:16:33 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy Birthday to me.`)
+// }
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts
+}
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+function countDown(pos_int) {
+    while (pos_int >= 0) {
+        console.log(pos_int)
+        pos_int--
+    }
+}
