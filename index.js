549c6b03f653cadac37b3072d22dc4f4f7b1ffce
AndrewMatheny <github email address>
2019-12-09 14:11:40 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards
+}
+
+function countDown(n) {
+    let count = n;
+    while (count > -1) {
+        console.log(count--);
+    }
+}
